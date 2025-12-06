f69f2828308d7ec850ddf7dab188cd7e60f518c1
Morzune11 <mocharrizal12@gmail.com>
2025-12-06 11:07:13 +0700

    show dialog for logout, clear history, clear cache

--- app/src/main/java/com/netown/semuabisa/features/profile/ProfileFragment.kt
+++ app/src/main/java/com/netown/semuabisa/features/profile/ProfileFragment.kt
@@ -1,60 +1,60 @@
 package com.netown.semuabisa.features.profile
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.netown.semuabisa.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+       val view =  inflater.inflate(R.layout.fragment_profile, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val btnEditProfile = view.findViewById<Button>(R.id.btnEditProfile)
+        val btnDriverFav = view.findViewById<LinearLayout>(R.id.btnDriverFav)
+        val btnClearCache = view.findViewById<LinearLayout>(R.id.btnClearCache)
+        val btnClearHistory = view.findViewById<LinearLayout>(R.id.btnClearHistory)
+        val btnLogout = view.findViewById<LinearLayout>(R.id.btnLogout)
+
+
+        btnClearCache.setOnClickListener {
+            showCustomDialog(R.layout.dialog_clear_cache)
+        }
+
+        btnClearHistory.setOnClickListener {
+            showCustomDialog(R.layout.dialog_clear_history)
+        }
+
+        btnLogout.setOnClickListener {
+            showCustomDialog(R.layout.dialog_logout)
+        }
+
+        return view
+    }
+    private fun showCustomDialog(layoutId: Int){
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(layoutId)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val btnCancel = dialog.findViewById<View>(R.id.btnCancel)
+        val btnYes = dialog.findViewById<View>(R.id.btnYes)
+
+        btnCancel?.setOnClickListener { dialog.dismiss() }
+        btnYes?.setOnClickListener {
+            Toast.makeText(requireContext(),"Confirmed!", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
